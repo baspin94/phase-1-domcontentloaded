b8b93427673a856f960f2a43b52d738d78a35032
Bianca Aspin <bianca@mj94.net>
2023-01-11 20:23:07 -0800

    Added event listener to complete lab.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.querySelector("#text");
+    p.textContent = "This is really cool!"
+});
+
+console.log("I'm still loading.");
